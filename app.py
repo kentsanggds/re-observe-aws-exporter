62f7dfceb97798212d3bcfe30bf7ed294b35ac08
Sam Crang <sam.crang@gmail.com>
2018-07-03 10:53:32 +0100

    Add EBS available and in-use count

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ from prometheus_client import start_http_server, Gauge
 PORT = 8000
 INTERVAL_IN_SECONDS = 1
 
+class EBSVolumeCount(object):
+    def __init__(self, name, desc=''):
+        self.name = name
+        self.desc = desc
+        self.total = Gauge(self.name + '_total', self.desc)
+        self.in_use = Gauge(self.name + '_in_use', self.desc)
+        self.ec2 = boto3.resource('ec2')
+
+    def emit(self):
+        all_volumes = list(self.ec2.volumes.all())
+
+        in_use_count = 0
+        for volume in all_volumes:
+            if volume.state == 'in-use':
+                in_use_count += 1
+
+        self.total.set(len(all_volumes))
+        self.in_use.set(in_use_count)
+
 class S3BucketCount(object):
     def __init__(self, name, desc=''):
         self.name = name
@@ -31,7 +50,8 @@ class RandomNumber(object):
 
 metrics = [
     RandomNumber('random_number', 'A random number'),
-    S3BucketCount('s3_bucket_count', 'The total number of s3 Buckets')
+    EBSVolumeCount('ebs_volumes', 'lol'),
+    #S3BucketCount('s3_bucket_count', 'The total number of s3 Buckets')
 ]
 
 
