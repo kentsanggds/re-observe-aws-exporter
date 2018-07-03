34b48bf78a53ae4bea591d46f3808cd3e8c01d6c
Sam Crang <sam.crang@gmail.com>
2018-07-03 13:35:41 +0100

    Make names more consistent

--- app.py
+++ app.py
@@ -7,50 +7,50 @@ from prometheus_client import start_http_server, Gauge
 PORT = 8000
 INTERVAL_IN_SECONDS = 1
 
-class EIPCount(object):
-    def __init__(self, name, desc=''):
-        self.name = name
+class EIPTotal(object):
+    def __init__(self, prefix='eip', desc='Total number of EIPs'):
+        self.prefix = prefix
         self.desc = desc
-        self.total = Gauge(self.name + '_total', self.desc)
-        self.in_use = Gauge(self.name + '_in_use', self.desc)
+        self.total = Gauge(self.prefix + '_total', self.desc)
+        self.in_use_total = Gauge(self.prefix + '_in_use_total', self.desc)
         self.ec2 = boto3.client('ec2')
 
     def emit(self):
         all_ips = self.ec2.describe_addresses()['Addresses']
 
-        in_use_count = 0
+        in_use_total = 0
         for ip in all_ips:
             if 'AssociationId' in ip:
-                in_use_count += 1
+                in_use_total += 1
 
         self.total.set(len(all_ips))
-        self.in_use.set(in_use_count)
+        self.in_use_total.set(in_use_total)
 
 
-class EBSVolumeCount(object):
-    def __init__(self, name, desc=''):
-        self.name = name
+class EBSVolumeTotal(object):
+    def __init__(self, prefix='ebs_volumes', desc='Total number of EBS volumes'):
+        self.prefix = prefix
         self.desc = desc
-        self.total = Gauge(self.name + '_total', self.desc)
-        self.in_use = Gauge(self.name + '_in_use', self.desc)
+        self.total = Gauge(self.prefix + '_total', self.desc)
+        self.in_use_total = Gauge(self.prefix + '_in_use_total', self.desc)
         self.ec2 = boto3.resource('ec2')
 
     def emit(self):
         all_volumes = list(self.ec2.volumes.all())
 
-        in_use_count = 0
+        in_use_total = 0
         for volume in all_volumes:
             if volume.state == 'in-use':
-                in_use_count += 1
+                in_use_total += 1
 
         self.total.set(len(all_volumes))
-        self.in_use.set(in_use_count)
+        self.in_use_total.set(in_use_total)
 
-class EBSEncryptedVolumeCount(object):
-    def __init__(self, name, desc=''):
-        self.name = name
+class EBSEncryptedVolumeTotal(object):
+    def __init__(self, prefix='ebs_volumes_encrypted', desc='Total number of encrypted EBS volumes'):
+        self.prefix = prefix
         self.desc = desc
-        self.encrypted_total = Gauge(self.name + '_total', self.desc)
+        self.encrypted_total = Gauge(self.prefix + '_total', self.desc)
         self.ec2 = boto3.resource('ec2')
 
     def emit(self):
@@ -64,34 +64,34 @@ class EBSEncryptedVolumeCount(object):
         self.encrypted_total.set(encrypted_count)
 
 
-class S3BucketCount(object):
-    def __init__(self, name, desc=''):
-        self.name = name
+class S3BucketTotal(object):
+    def __init__(self, prefix='s3_bucket', desc='Total number of S3 Buckets'):
+        self.prefix = prefix
         self.desc = desc
-        self.gauge = Gauge(self.name, self.desc)
+        self.total= Gauge(self.prefix + "total", self.desc)
         self.s3 = boto3.resource('s3')
 
     def emit(self):
         bucket_count = len(list(self.s3.buckets.all()))
-        self.gauge.set(bucket_count)
+        self.total.set(bucket_count)
 
 
 class RandomNumber(object):
-    def __init__(self, name, desc=''):
-        self.name = name
+    def __init__(self, prefix='random_number', desc='A random number'):
+        self.prefix = prefix
         self.desc = desc
-        self.gauge = Gauge(self.name, self.desc)
+        self.gauge = Gauge(self.prefix, self.desc)
 
     def emit(self):
         self.gauge.set(random.random())
 
 
 metrics = [
-    RandomNumber('random_number', 'A random number'),
-    EIPCount('eip_count', 'EIP metrics'),
-    EBSVolumeCount('ebs_volumes', 'EBS metrics'),
-    EBSEncryptedVolumeCount('ebs_encrypted_volumes', 'Encrypted EBS metrics'),
-    S3BucketCount('s3_bucket_count', 'The total number of s3 Buckets')
+    RandomNumber(),
+    EIPTotal(),
+    EBSVolumeTotal(),
+    EBSEncryptedVolumeTotal(),
+    S3BucketTotal()
 ]
 
 
